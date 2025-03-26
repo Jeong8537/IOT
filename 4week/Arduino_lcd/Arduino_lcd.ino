bb97da5ca7256c03ced74dcdc6c87ae4f11b3940
Jeong8537 <jeong85373546@gmail.com>
2025-03-26 10:51:30 +0900

    4week | Arduino(lcd)!

---
+++ 4week/Arduino_lcd/Arduino_lcd.ino
@@ -0,0 +1,27 @@
+#include <LiquidCrystal_I2C.h>
+
+#include <Wire.h>
+
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.print("LCD init");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop()
+{
+  lcd.setCursor(16, 0);
+  lcd.print(" /\\   /\\ ");
+  lcd.setCursor(16,1);
+  lcd.print("( > . - )");
+    for (int position =0; position < 16; position++){
+      lcd.scrollDisplayLeft();
+      delay(500);
+  }
+}
